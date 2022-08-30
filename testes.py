65dfaea1ed99f3e8b2c9c4d4dd3e5c6053d680d1
Renato Alves Soares <renatoguara2020@gmail.com>
2022-08-30 20:31:42 -0300

    commit mysql changes

--- testes.py
+++ testes.py
@@ -1,3 +1,10 @@
-fruits = ['mango', 'kiwi', 'grape', 'melon']
+fruits = ['mango', 'kiwi', 'grape', 'melon', 'banana']
 arr_length = len(fruits)
 print(arr_length)
+
+
+e = "renato"
+
+x = "renato" in e
+
+print(x)
