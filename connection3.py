c46d3c8936f229f8d1fc0102cccbbcae31bea5e2
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-04 12:11:07 -0300

    commit changes

---
+++ connection3.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+connection = mysql.connector.connect(host="localhost",
+                                     user="root",
+                                     passwd="",
+                                     db="db")
+
+try:
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        db = cursor.fetchone()
+        print("Você está conectado ao banco de dados: ", db)
+except Error as e:
+    print("Erro ao conectar ao MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("A conexão MySQL está fechada")
