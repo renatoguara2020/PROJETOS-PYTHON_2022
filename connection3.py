0182a2fcef0077061867248f27dc66dc98f02e33
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-04 12:18:28 -0300

    commit changes

--- connection3.py
+++ connection3.py
@@ -4,14 +4,14 @@ from mysql.connector import Error
 connection = mysql.connector.connect(host="localhost",
                                      user="root",
                                      passwd="",
-                                     db="db")
+                                     db="bd")
 
 try:
     if connection.is_connected():
         cursor = connection.cursor()
         cursor.execute("select database();")
         db = cursor.fetchone()
-        print("Você está conectado ao banco de dados: ", db)
+        print("Você está conectado ao banco de dados: ", bd)
 except Error as e:
     print("Erro ao conectar ao MySQL", e)
 finally:
